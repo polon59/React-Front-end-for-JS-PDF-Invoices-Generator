0adae0452b7358df5efe485178c003c01e8426b6
polon59 <przemyslaw.polczak@gmail.com>
2019-02-19 19:54:26 +0100

    Add invoices list in App component state

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './navbar';
+import InvoicesList from './InvoicesList';
 
 
 class App extends Component {
+  state = {
+    invoices : [
+      {id: 1, title: "invoice1", billTo:"bill to one"},
+      {id: 2, title: "invoice2", billTo:"bill to two"},
+      {id: 3, title: "invoice3", billTo:"bill to three"},
+      {id: 4, title: "invoice4", billTo:"bill to four"}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +21,8 @@ class App extends Component {
           <p>
             React application
           </p>
+
+          <InvoicesList invoices={this.state.invoices}/>
         </header>
       </div>
     );
