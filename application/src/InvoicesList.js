d94fa468cf3cab60931d1c61b512e43aca5d0e44
polon59 <przemyslaw.polczak@gmail.com>
2019-02-19 20:19:23 +0100

    Change InvoicesList to become a functionalComponent instead of class component

--- application/src/InvoicesList.js
+++ application/src/InvoicesList.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-class InvoicesList extends Component{
-
-    render(){
-        const { invoices } = this.props;
-        const invoicesList = invoices.map(invoice =>{
-            return (
-                <tr key={invoice.id}>
-                    <td>{invoice.id}</td>
-                    <td>{invoice.title}</td>
-                    <td>{invoice.billTo}</td>
-                </tr>
-            );
-        });
-
-        return(
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>TITLE</th>
-                            <th>BILL TO</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {invoicesList}
-                    </tbody>
-                </table>
-                
-            </div>
+const InvoicesList = (props)=>{
+    const { invoices } = props;
+    const invoicesList = invoices.map(invoice =>{
+        return (
+            <tr key={invoice.id}>
+                <td>{invoice.id}</td>
+                <td>{invoice.title}</td>
+                <td>{invoice.billTo}</td>
+            </tr>
         );
-    }
+    });
+
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>BILL TO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {invoicesList}
+                </tbody>
+            </table>
+            
+        </div>
+    );
 }
 
 export default InvoicesList;
