7ee1fe8d158a45e8d36aca0e801d9cb3abab66d5
polon59 <przemyslaw.polczak@gmail.com>
2019-02-19 17:27:25 +0100

    Add new simple component - navbar.js

--- application/src/App.js
+++ application/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './navbar';
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Navbar />
           <p>
             React application
           </p>
---
+++ application/src/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component{
+    render(){
+        return(
+            <div>
+                <div>Option 1</div>
+                <div>Description 1</div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
